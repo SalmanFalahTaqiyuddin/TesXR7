ee81a23ca5bdb8ea5a29dfdfb27a5d97fbfca3e0
Salman Falah Taqiyuddin <salman_taqiyuddin_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:36:47 +0700

    Add files via upload

---
+++ data.java
@@ -0,0 +1,14 @@
+public class data {
+    public static void main(String[] args) {
+        //tipe_data nama_var = value/isi var;
+      String nama = "Salman";
+      int usia = 15;
+      String alamat = "Jl. Danau Buyan, Sawojajar, Kec. Kedungkandang, Kota Malang, Jawa Timur 65139";
+      double kelas = 10.7;
+
+      System.out.println("Nama saya : " + nama);
+      System.out.println("Usia : " + usia + " tahun");
+      System.out.println("Alamat : " + alamat);
+      System.out.println("Kelas : " + kelas);
+    }
+}
